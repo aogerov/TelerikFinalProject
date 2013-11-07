09fc5f4a3c6e0925175125106e33ae48f103e8cc
antonios123 <otr@abv.bg>
2013-11-07 19:29:38 +0200

    navigation drawer added

--- FindMyBuddiesProject/FindMyBuddies/src/main/java/com/gercho/findmybuddies/MainActivity.java
+++ FindMyBuddiesProject/FindMyBuddies/src/main/java/com/gercho/findmybuddies/MainActivity.java
@@ -1,144 +1,83 @@
 package com.gercho.findmybuddies;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+import com.gercho.findmybuddies.helpers.NavigationDrawerHelper;
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+public class MainActivity extends FragmentActivity implements ListView.OnItemClickListener {
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+    public static final String EXTRA_COURSE_LIB = "course lib";
+
+    private static final int COURSE_LIB_NOT_SET = -1;
+
+//    private CoursePagerAdapter mCoursePagerAdapter;
+    private ViewPager mViewPager;
+    private NavigationDrawerHelper mNavigationDrawerHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+//        this.mCoursePagerAdapter = new CoursePagerAdapter(
+//                this.getSupportFragmentManager(), this);
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
+//        this.mViewPager = (ViewPager) findViewById(R.id.pager);
+//        this.mViewPager.setAdapter(this.mCoursePagerAdapter);
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
+        this.mNavigationDrawerHelper = new NavigationDrawerHelper();
+        this.mNavigationDrawerHelper.init(this, this);
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
+        Intent startupIntent = this.getIntent();
+        int courseLib = startupIntent.getIntExtra(EXTRA_COURSE_LIB, COURSE_LIB_NOT_SET);
+        if (courseLib != COURSE_LIB_NOT_SET) {
+//            this.mCoursePagerAdapter.setCourseLib(courseLib);
+            this.mNavigationDrawerHelper.setSelection(courseLib);
         }
     }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        this.getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int optionsLib, long l) {
+//        this.mCoursePagerAdapter.setCourseLib(optionsLib);
+        this.mNavigationDrawerHelper.handleSelect(optionsLib);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.mNavigationDrawerHelper.syncState();
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        this.mNavigationDrawerHelper.handleOnPrepareOptionsMenu(menu);
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
+        this.mNavigationDrawerHelper.handleOnOptionsItemSelected(item);
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        this.mNavigationDrawerHelper.syncState();
     }
-
 }
---
+++ FindMyBuddiesProject/FindMyBuddies/src/main/java/com/gercho/findmybuddies/helpers/NavigationDrawerHelper.java
@@ -0,0 +1,96 @@
+package com.gercho.findmybuddies.helpers;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.gercho.findmybuddies.R;
+
+/**
+ * Created by Gercho on 11/7/13.
+ */
+public class NavigationDrawerHelper {
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    public void init(Activity theActivity, ListView.OnItemClickListener listener) {
+        this.mDrawerLayout = (DrawerLayout) theActivity.findViewById(R.id.drawer_layout);
+        this.mDrawerListView = (ListView) theActivity.findViewById(R.id.left_drawer);
+
+        this.mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        String[] navigationDrawerOptions =
+                theActivity.getResources().getStringArray(R.array.navigation_drawer_options);
+        ArrayAdapter<String> navigationDrawerAdapter =
+                new ArrayAdapter<String>(theActivity, R.layout.drawer_option_item, navigationDrawerOptions);
+
+        this.mDrawerListView.setAdapter(navigationDrawerAdapter);
+        this.mDrawerListView.setOnItemClickListener(listener);
+//        this.mDrawerListView.setItemChecked(CoursePagerAdapter.COURSE_LIB_ANDROID, true);
+
+        this.setupActionBar(theActivity);
+    }
+
+    public void handleSelect(int option) {
+        this.mDrawerListView.setItemChecked(option, true);
+        this.mDrawerLayout.closeDrawer(this.mDrawerListView);
+    }
+
+    public void handleOnPrepareOptionsMenu(Menu menu) {
+        boolean isItemVisible = !this.mDrawerLayout.isDrawerOpen(this.mDrawerListView);
+
+        for (int index = 0; index < menu.size(); index++) {
+            MenuItem item = menu.getItem(index);
+            item.setEnabled(isItemVisible);
+        }
+    }
+
+    public void handleOnOptionsItemSelected(MenuItem item) {
+        this.mDrawerToggle.onOptionsItemSelected(item);
+    }
+
+    public void syncState() {
+        this.mDrawerToggle.syncState();
+    }
+
+    public void setSelection(int option) {
+        this.mDrawerListView.setItemChecked(option, true);
+    }
+
+    private void setupActionBar(Activity theActivity) {
+        final Activity activity = theActivity;
+
+        // this works on API 11 and above
+        ActionBar actionBar = theActivity.getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        this.mDrawerToggle = new ActionBarDrawerToggle(
+                theActivity,
+                this.mDrawerLayout,
+                R.drawable.ic_drawer,
+                R.string.open_drawer_message,
+                R.string.close_drawer_message
+        ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                activity.invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                activity.invalidateOptionsMenu();
+            }
+        };
+    }
+}
